2a9a6d4aeaeaef8a9892b13b2da1d8c6165b5ba2
Mike <mike@X1carbon>
2025-02-25 17:59:06 +0100

    Restore point

--- src/file1.rs
+++ src/file1.rs
@@ -1,12 +1,48 @@
-fn main() {
-    // filename: file1.rs
-    for n in 1..26 {
-        let m = 10000 - 396 * n;
-        let p: f64 = (m as f64).sqrt();
-        let q: f64 = p.floor();
-        if p - q == 0.0 {
-            println!("n={0} : determinant = {1} ", n, m);
-        }
+pub fn func1(){
+
+    println!("module func1");
+    for n in 1..100 {
+        print!("{} ", n);
+        println!("{}", n*n);
+    }
+
+}
+
+pub fn func2(n: u32){
+    println!("module func2");
+    let mut d = 10;
+    loop {
+        print!("{}  ", n % d);
+        d = d*10;
+        if n % d == n { break;}; 
     }
 }
 
+pub fn twenty_twentyfive(n: u32) -> bool {
+    let mut d = 10;
+    let sq = n * n;
+    let mut flag = false;
+
+    loop {
+        let mut lhs = sq / d;
+        let mut rhs = sq % d;
+        println!("lhs: {}  rhs: {}  sum: {}", lhs, rhs, lhs + rhs);
+        // TODO this should be the concatenation of lhs&rhs
+        while rhs > 0 {
+            lhs *= 10;
+            dbg!(lhs);
+            dbg!(rhs);
+            lhs = lhs + rhs/10;
+            rhs %= 10;
+        }
+
+        if lhs == n {
+            flag = true;
+            break;
+        }
+        
+        d *= 10;
+        if d > sq { break; }  // Ensuring termination
+    }
+    flag
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
+#[allow(dead_code)]
+mod file1;
+
 fn main() {
-    for n in 1..26 {
-        let m = 10000 - 396 * n;
-        let p: f64 = (m as f64).sqrt();
-        let q: f64 = p.floor();
-        if p - q == 0.0 {
-            println!("n={0} : determinant = {1} ", n, m);
+    // Found sequence A248353 in OEIS. Refered to as kaprekar Numbers.
+    //  n such that n=q+r and n^2=q*10^m+r, for some m >= 1, q>=0 and 0<=r<10^m.
+    let mut _foo : bool;
+    for n in 99..100 {
+         _foo = file1::twenty_twentyfive(n);
+         if _foo {
+            println!("{0} {1} {2}", n, n*n, _foo);
         }
     }
 }
-
