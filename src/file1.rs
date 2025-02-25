05fd7c0659a42c24e6d6a4de4b2f05a44731477a
Mike <mike@X1carbon>
2025-02-25 11:09:38 +0100

    Setting up github repo

---
+++ src/file1.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // filename: file1.rs
+    for n in 1..26 {
+        let m = 10000 - 396 * n;
+        let p: f64 = (m as f64).sqrt();
+        let q: f64 = p.floor();
+        if p - q == 0.0 {
+            println!("n={0} : determinant = {1} ", n, m);
+        }
+    }
+}
+
