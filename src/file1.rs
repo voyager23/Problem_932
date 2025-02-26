b01b2cd4de92d0096fc577be2b0cbc15a7543bda
Mike <mike@X1carbon>
2025-02-26 14:38:34 +0100

    Updated the concat() and digits() to take parameters.

--- src/file1.rs
+++ src/file1.rs
@@ -45,27 +45,25 @@ pub fn twenty_twentyfive(n: u32) -> bool {
     flag
 }
 
-pub fn concat() {
-    let mut ab : u32 = 9801;
+pub fn concat(n:u32) -> u32 {
+    let mut ab : u32 = n;
     let s : String = ab.to_string();
     let pq = &s[2..];
     
     let number: u32 = pq.parse().expect("Not a valid number");
     
     ab = (ab/100)*10 + number;
+    // println!("{}",ab);
     
-    println!("{}",ab);
+    ab
     
 }
 
-pub fn digits() -> Vec<(u32,u32)>{
-    let mut first : u32 = 0;
-    let mut end : u32 = 0;
+pub fn digits(lo:u32, hi:u32) -> Vec<(u32,u32)>{
     let mut limits : Vec<(u32,u32)> = vec![];
-
-    for digits in 2..5{
-        first = u32::pow(10,digits-1) + 1;
-        end = u32::pow(10,digits);
+    for digits in lo..hi+1{
+        let first : u32 = u32::pow(10,digits-1) + 1;
+        let end = u32::pow(10,digits);
         println!("digits:{}\tfirst:{}  end:{}", digits, first, end);
         limits.push((first,end));
     }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 #[allow(dead_code)]
 mod file1;
 
-fn main() {
+fn main() 
+{
     // Found sequence A248353 in OEIS. Refered to as kaprekar Numbers.
     //  n such that n=q+r and n^2=q*10^m+r, for some m >= 1, q>=0 and 0<=r<10^m.
-    let mut _limits : Vec<(u32,u32)> = vec![];
+
+    let _limits = file1::digits(1,2);
+    let mut _t : (u32,u32) = (42,42);
+    for t in  _limits 
+    { 
+       for x in t.0 .. t.1 {
+            println!("{}",x);
+       }
+    }
+
+    //let x : u32 = file1::concat(9801);
+    //println!("9801 -> {}", file1::concat(9801) );
+}
+
+
+
+/*
+Scratch pad area
+let mut _limits : Vec<(u32,u32)> = vec![];
     let mut _foo : bool;
     for n in 99..100 {
          _foo = file1::twenty_twentyfive(n);
@@ -12,11 +31,4 @@ fn main() {
             println!("{0} {1} {2}", n, n*n, _foo);
         }
     }
-
-    let _limits = file1::digits();
-    let mut _t : (u32,u32) = (42,42);
-    for _t in  _limits 
-    { 
-        println!("{} {}", _t.0, _t.1);
-    }
-}
+*/
